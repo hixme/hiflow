7e9ef38c4b9c0c86efa2d976c577c78508aed7cb
Billy Montgomery <billy.montgomery@gmail.com>
2017-12-24 23:24:00 -0800

    feature/login: setup cli for config and prs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
+import inquirer from 'inquirer';
+
 import { _, command, args } from './args';
+import { runSetup } from './config'
+import { promptPullRequestList } from './bitbucket'
 
 switch (command) {
+  case 'config':
+    runSetup()
+    break
+  case 'prs':
+    promptPullRequestList()
+    break
   default:
     console.log('hello', command)
 }
